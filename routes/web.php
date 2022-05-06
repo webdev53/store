274d767eeabd3c8c992f8f6bc64a9b4331caa5e9
Web Dev <web.2019.dev@gmail.com>
2022-05-06 21:48:30 +0545

    home and about page added

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $viewData = [];
+    $viewData['title'] = "Home Page - Online Store";
+    return view('home.index')->with("viewData", $viewData);
 });
+
+Route::get('/about','App\Http\Controllers\HomeController@about')->name('home.about');
